cc260e1dc2a4bf30f4996e8165eb101c84768674
Sathiskumar2113 <sathiskumar2112000@gmail.com>
2024-07-31 10:48:25 +0530

    useref used

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const intputref =useRef(null);
+  const intputrefe =useRef(null);
+
+  function handleclick()
+  {
+    // console.log(intputref.current.value);
+     intputref.current.focus();
+  }
+
+  function handleclicke()
+  {
+    
+     intputrefe.current.focus();
+  }
+
+  function handlechange()
+  {
+    intputrefe.current.value="sathis";
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+     <input type='text' ref={intputref} ></input>
+     <input type='text' ref={intputrefe}></input>
+     <button onClick={handleclick}>click</button>
+     <button onClick={handleclicke}>clicke</button>
+     <button onClick={handlechange}>change</button>
     </div>
   );
 }
